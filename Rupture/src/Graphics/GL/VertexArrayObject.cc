e420e9e908576812c2b65a4b40d90ecef5174147
lesedow <lesedow@gmail.com>
2026-09-20 21:29:00 +0300

    VAO implementation

---
+++ Rupture/src/Graphics/GL/VertexArrayObject.cc
@@ -0,0 +1,25 @@
+#include "Graphics/GL/VertexArrayObject.hh"
+#include "Utils/Macros/GLMacros.hh"
+
+namespace Rupture::Graphics::GL
+{
+	VertexArrayObject::VertexArrayObject()
+	{
+		RP_GL(glGenVertexArrays(1, &ID_));
+	}
+
+	VertexArrayObject::~VertexArrayObject()
+	{
+		RP_GL(glDeleteVertexArrays(1, &ID_));
+	}
+
+	void VertexArrayObject::Bind() const
+	{
+		RP_GL(glBindVertexArray(ID_));
+	}
+
+	void VertexArrayObject::Unbind() const
+	{
+		RP_GL(glBindVertexArray(0));
+	}
+}
