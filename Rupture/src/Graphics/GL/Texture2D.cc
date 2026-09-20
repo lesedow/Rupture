bc71464a5f6591eca24068af50bc2cc4846ee238
lesedow <lesedow@gmail.com>
2026-09-20 21:28:48 +0300

    Shortened the macro name

--- Rupture/src/Graphics/GL/Texture2D.cc
+++ Rupture/src/Graphics/GL/Texture2D.cc
@@ -15,25 +15,25 @@ namespace Rupture::Graphics::GL
 		int width, height, channels;
 		unsigned char* imageData = stbi_load(path.c_str(), &width, &height, &channels, 0);
 
-		if (!imageData) RUPTURE_LOG_FATAL(std::format("Failed to load image: {}", path));
+		if (!imageData) RP_LOG_ERROR(std::format("Failed to load image: {}", path));
 
 		size_ = glm::vec2(width, height);
 		GLenum pixelFormat = channels == 4 ? GL_RGBA : GL_RGB;
 
-		RUPTURE_GL_CALL(glGenTextures(1, &id_));
-		RUPTURE_GL_CALL(glBindTexture(GL_TEXTURE_2D, id_));
+		RP_GL(glGenTextures(1, &id_));
+		RP_GL(glBindTexture(GL_TEXTURE_2D, id_));
 
-		RUPTURE_GL_CALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT));
-		RUPTURE_GL_CALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT));
-		RUPTURE_GL_CALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST));
-		RUPTURE_GL_CALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST));
+		RP_GL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT));
+		RP_GL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT));
+		RP_GL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST));
+		RP_GL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST));
 
-		RUPTURE_GL_CALL(glTexImage2D(
+		RP_GL(glTexImage2D(
 			GL_TEXTURE_2D, 0, pixelFormat, width, 
 			height, 0, pixelFormat, GL_UNSIGNED_BYTE, imageData
 		));
 
-		RUPTURE_GL_CALL(glGenerateMipmap(GL_TEXTURE_2D));
+		RP_GL(glGenerateMipmap(GL_TEXTURE_2D));
 
 		stbi_image_free(imageData);
 	}
@@ -45,7 +45,7 @@ namespace Rupture::Graphics::GL
 
 	void Texture2D::Bind(GLenum slot)
 	{
-		RUPTURE_GL_CALL(glActiveTexture(slot));
-		RUPTURE_GL_CALL(glBindTexture(GL_TEXTURE_2D, id_));
+		RP_GL(glActiveTexture(slot));
+		RP_GL(glBindTexture(GL_TEXTURE_2D, id_));
 	}
 }
