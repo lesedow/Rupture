872806edd032f364c9dd4f544807c00aea5ba7fb
lesedow <lesedow@gmail.com>
2026-09-20 21:26:46 +0300

    Chnaged some stuff here and there

--- Rupture/include/Utils/GL/GlError.hh
+++ Rupture/include/Utils/GL/GlError.hh
@@ -1,28 +1,27 @@
 #pragma once
 
+#include "Graphics/GL/GLTypes.hh"
 #include <string>
 
 namespace Rupture::Utils::GL
-{
+{ 
 	inline void ClearGLErrors()
 	{
-		namespace gl = Rupture::Graphics::GL;
-		while (gl::glGetError() != gl::GL_NO_ERROR);
+		while (Graphics::GL::glGetError() != Graphics::GL::GL_NO_ERROR);
 	}
 
 	inline std::string GetGLErrors()
 	{
-		namespace gl = Rupture::Graphics::GL;
 		std::string errors{};
-		while (GLenum error = gl::glGetError())
+		while (Graphics::GL::GLenum error = Graphics::GL::glGetError())
 		{
 			switch (error)
 			{
-			case gl::GL_INVALID_ENUM: errors.append("INVALID ENUM\n"); break;
-			case gl::GL_INVALID_VALUE: errors.append("INVALID VALUE\n"); break;
-			case gl::GL_INVALID_OPERATION: errors.append("INVALID OPERATION\n"); break;
-			case gl::GL_INVALID_FRAMEBUFFER_OPERATION: errors.append("INVALID FRAMEBUFFER OPERATION\n"); break;
-			case gl::GL_OUT_OF_MEMORY: errors.append("OUT OF MEMORY\n"); break;
+			case Graphics::GL::GL_INVALID_ENUM: errors.append("INVALID ENUM\n"); break;
+			case Graphics::GL::GL_INVALID_VALUE: errors.append("INVALID VALUE\n"); break;
+			case Graphics::GL::GL_INVALID_OPERATION: errors.append("INVALID OPERATION\n"); break;
+			case Graphics::GL::GL_INVALID_FRAMEBUFFER_OPERATION: errors.append("INVALID FRAMEBUFFER OPERATION\n"); break;
+			case Graphics::GL::GL_OUT_OF_MEMORY: errors.append("OUT OF MEMORY\n"); break;
 			}
 		}
 
