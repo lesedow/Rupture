f50dd4113d630dc4603041f0d93536785212a505
lesedow <lesedow@gmail.com>
2026-09-20 21:26:25 +0300

    Basic 2D renderer class

---
+++ Rupture/include/Rendering/GLRenderer.hh
@@ -0,0 +1,50 @@
+#pragma once
+
+#include "Graphics/GL/GLTypes.hh"
+#include "Graphics/GL/Texture2D.hh"
+#include "Graphics/GL/GLBuffer.hh"
+#include "Graphics/GL/VertexArrayObject.hh"
+#include "Graphics/GL/ShaderProgram.hh"
+
+#include <vector>
+#include <glm/glm.hpp>
+
+namespace Rupture::Rendering
+{
+	struct Vertex
+	{
+		glm::vec3 Position;
+		glm::vec4 Color;
+		glm::vec2 TextureCoords;
+		std::uint32_t TextureId;
+	};
+
+	class GLRenderer
+	{
+	private:
+		static constexpr Graphics::GL::GLsizeiptr BATCH_CAPACITY		= 0x2710LL;
+		static constexpr Graphics::GL::GLsizeiptr MAX_QUADS				= BATCH_CAPACITY / 4;
+		static constexpr Graphics::GL::GLint INDICES_PER_QUAD			= 6;
+		static constexpr Graphics::GL::GLsizeiptr MAX_INDICES			= MAX_QUADS * INDICES_PER_QUAD;
+		static constexpr Graphics::GL::GLint MAX_ACTIVE_TEXTURES		= 32;
+	private:
+		std::vector<Vertex> Vertices_;
+		std::vector<std::uint32_t> Indices_;
+		Graphics::GL::VertexArrayObject VertexArrayObject_;
+		Graphics::GL::GLBuffer VertexBufferObject_;
+		Graphics::GL::GLBuffer ElementBufferObject_;
+		Graphics::GL::ShaderProgram ShaderProgram_;
+		Graphics::GL::GLuint QuadCount_;
+	private:
+		void GenIndices();
+	public:
+		GLRenderer();
+		~GLRenderer();
+	public:
+		void StartBatch();
+		void EndBatch();
+		void DrawQuad(glm::vec2 position, glm::vec2 size, glm::vec4 color);
+		void DrawCircle(glm::vec2 position, Graphics::GL::GLfloat radius, glm::vec4 color);
+		void DrawTexture(const Graphics::GL::Texture2D& texture, glm::vec2 position, glm::vec2 size, glm::vec4 tint);
+	};
+}
