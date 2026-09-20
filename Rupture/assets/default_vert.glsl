80df219ed20b1e68f64c472aa86f168f2a53b855
lesedow <lesedow@gmail.com>
2026-09-20 21:18:49 +0300

    change some names here

--- Rupture/assets/default_vert.glsl
+++ Rupture/assets/default_vert.glsl
@@ -5,14 +5,14 @@ layout (location = 1) in vec4 aColor;
 layout (location = 2) in vec2 aTexCoord;
 layout (location = 3) in float aTexID;
 
-uniform mat4 view;
-uniform mat4 projection;
-uniform mat4 model;
+uniform mat4 View;
+uniform mat4 Projection;
+uniform mat4 Model;
 
 out vec2 TexCoord;
 
 void main()
 {
-	gl_Position = projection * view * model * vec4(aPos, 1.0);
+	gl_Position = Projection * View * Model * vec4(aPos, 1.0);
 	TexCoord = aTexCoord;
 }
